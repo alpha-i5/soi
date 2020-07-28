323cb93bd6b455abe8f7883496ab8c255c8508a6
alpha-i5 <ywgyg497@gmail.com>
2020-07-28 12:33:55 +0900

    soi_ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#index'
   get 'users/index'
   get 'users/update'
   get 'users/edit'
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
